96bd7585e5a787254f4a2139368d2a8bc30b5462
sol <sol.grana13@gmail.com>
2024-12-12 12:29:46 -0300

    Actualiza Configuracion app.py para deployment

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,32 @@
-from flask import Flask,send_from_directory
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-frontend_folder = os.path.join(os.getcwd(),"..","frontend")
-dist_folder = os.path.join(frontend_folder,"dist")
+# Rutas para el frontend
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
 
-# Server static files from the "dist" folder under the "frontend" directory
-@app.route("/",defaults={"filename":""})
+@app.route("/", defaults={"filename": ""})
 @app.route("/<path:filename>")
 def index(filename):
-  if not filename:
-    filename = "index.html"
-  return send_from_directory(dist_folder,filename)
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
 
-#api routes
+# Importa las rutas de la API
 import routes
 
 with app.app_context(): 
-   db.create_all()
-   
+    db.create_all()
+
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
